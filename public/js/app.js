64e8ba8d378fb01ac0670401424a6b67bc252c0f
Timothy Fisher <tfisher9180@gmail.com>
2017-02-22 12:36:43 -0700

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -10,49 +10,95 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 		templateUrl: '/custom/views/home.html',
 		controller: 'MainCtrl'
 	}).
+	when('/:site', {
+		templateUrl: '/custom/views/view.html',
+		controller: 'ViewCtrl'
+	}).
 	otherwise({
 		redirectTo: '/'
 	});
 
 }]);
 
+app.filter('capitalize', function() {
+	return function(input) {
+		return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+	}
+});
+
 /* factory to return $resource object with RESTful methods */
-app.factory('Student', function($resource) {
+app.factory('Student', ['$resource', function($resource) {
 
 	/* prefix @ means id paramater will be in $scope._id (for MongoDB) */
 	return $resource('/api/students/:id', {id: '@_id'});
 
-});
+}]);
 
-app.filter('filterSite', function() {
-	return function(students, site) {
-		var i = 0, len=students.length;
-		for (i; i<len; i++) {
-			if (students[i].site == site) {
-				return students[i];
-			}
+app.factory('Count', [function(Student) {
+
+	return {
+		getCountBySite: function(data, sites) {
+
+			var counts = {};
+			counts.total = data.length
+			angular.forEach(sites, function(site) {
+				if (data.length > 0) {
+					var count = 0;
+					for (var i = 0; i < data.length; i++) {
+						if (data[i].site == site) {
+							count++;
+						}
+					}
+					counts[site] = count;
+				} else {
+					counts[site] = 0;
+				}
+			});
+			return counts;
+
 		}
-		return null;
 	};
-});
 
-app.controller('MainCtrl', ['$scope', 'Student', function($scope, Student) {
+}]);
+
+app.controller('MainCtrl', ['$scope', 'Count', 'Student', function($scope, Count, Student) {
 	$scope.student = new Student();
 	$scope.student.site = "";
-	$scope.sites = ['avondale', 'queen creek', 'northern', 'tempe', 'surprise', 'southern', 'thomas'];
+	$scope.sites = {
+		'Avondale': 'avondale', 
+		'Queen Creek': 'queen_creek', 
+		'Northern': 'northern', 
+		'Tempe': 'tempe', 
+		'Surprise': 'surprise', 
+		'Southern': 'southern', 
+		'Thomas': 'thomas'
+	}
 
 	Student.query({fields: 'site'}, function(data) {
-		$scope.total = data.length;
-		$scope.students = data;
+		$scope.counts = Count.getCountBySite(data, $scope.sites);
 	});
 
 	$scope.add = function() {
 		$scope.student.$save(function() {
+			Materialize.toast('Student added!', 3000);
 			$scope.student.fname = '';
 			$scope.student.lname = '';
 			$scope.student.site = '';
 			$scope.addForm.$setPristine();
+
+			Student.query({fields: 'site'}, function(data) {
+				$scope.counts = Count.getCountBySite(data, $scope.sites);
+			});
 		});
 	};
+}]);
+
+app.controller('ViewCtrl', ['$scope', '$routeParams', 'Student', function($scope, $routeParams, Student) {
+	var site = $routeParams.site;
+	$scope.site = site;
+
+	Student.query({site: site}, function(data) {
+		$scope.students = data;
+	});
 }]);
 
